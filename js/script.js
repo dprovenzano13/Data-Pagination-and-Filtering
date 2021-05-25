f3c554b3b553f869315735d61c4b863317b451b2
David Provenzano <dprovenzano13@gmail.com>
2021-05-24 22:21:49 -0400

    Created showPage Function

--- js/script.js
+++ js/script.js
@@ -11,13 +11,39 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
+// console.log(data);
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const perPage = 9;
+const studentList = document.querySelector('.student-list');
 
+
+function showPage(list, page) {
+  const startIndex = (page * perPage) - perPage;
+  const endIndex = (page * perPage);
+  studentList.innerHTML = '';
+
+  for (let i = 0; i < list.length; i++) {
+
+    if (i >= startIndex && i < endIndex) {
+      const studentItem =
+      `<li class="student-item cf">
+        <div class="student-details">
+          <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+          <h3>${list[i].name.first} ${list[i].name.last}</h3>
+          <span class="email">${list[i].email}</span>
+        </div>
+        <div class="joined-details">
+          <span class="date">Joined ${list[i].registered.date}</span>
+        </div>
+      </li>`;
+      studentList.insertAdjacentHTML('beforeend', studentItem);
+    }
+  }
+}
 
 
 /*
