79d3cc59efaf6d094d182ad67685e0fe850cedd8
David Provenzano <dprovenzano13@gmail.com>
2021-05-30 01:31:25 -0400

    Modify and complete the addPagination function

--- js/script.js
+++ js/script.js
@@ -9,60 +9,64 @@ const studentList = document.querySelector('.student-list');
 const linkList = document.querySelector('.link-list');
 const header = document.querySelector('.header');
 /*
- * Dynamically inserts 9 students per page.
- * It will create as many pages needed to fit the amount of students there are
- */
+* Dynamically inserts 9 students per page.
+* It will create as many pages needed to fit the amount of students there are
+*/
 function showPage(list, page = 1) {
   const startIndex = (page * perPage) - perPage;
   const endIndex = (page * perPage);
   studentList.innerHTML = '';
   for (let i = 0; i < list.length; i++) {
     if (i >= startIndex && i < endIndex) {
-    const studentItem =
-    `<li class="student-item cf">
-      <div class="student-details">
-        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
-        <h3>${list[i].name.first} ${list[i].name.last}</h3>
-        <span class="email">${list[i].email}</span>
-      </div>
-      <div class="joined-details">
-        <span class="date">Joined ${list[i].registered.date}</span>
-      </div>
-    </li>`;
-    studentList.insertAdjacentHTML('beforeend', studentItem);
+      const studentItem =
+        `<li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined ${list[i].registered.date}</span>
+          </div>
+        </li>`;
+      studentList.insertAdjacentHTML('beforeend', studentItem);
     }
   }
-// addPagination(list);
 }
 /*
- * Decides how many buttons are needed based off of how many search results
- * are found.
- */
+* Decides how many buttons are needed based off of how many search results
+* are found.
+*/
 function addPagination(list) {
-  const numOfPages = Math.ceil(list.length / perPage);
-  linkList.innerHTML = '';
-  for (let i = 1; i <= numOfPages; i++) {
-    const newButton =
-      `<li>
-      <button id="pagButton" type="button">${i}</button>
-    </li>`
-    linkList.insertAdjacentHTML('beforeend', newButton);
-  }
-  const buttonClass = document.querySelector('#pagButton');
-  buttonClass.className = 'active';
-/*
- * Listens for button clicks at the bottom of the page and changes the class to
- * 'active' each time a button is clicked.
- */
-  linkList.addEventListener('click', (e) => {
-    const clickButton = e.target;
-    if (clickButton.tagName === 'BUTTON') {
-      const firstActiveButton = document.querySelector('button.active');
-      firstActiveButton.classList.remove('active');
-      clickButton.className = 'active';
-      showPage(list, clickButton.textContent)
+  if (list.length !== 0) {
+    const numOfPages = Math.ceil(list.length / perPage);
+    linkList.innerHTML = '';
+    for (let i = 1; i <= numOfPages; i++) {
+      const newButton =
+        `<li>
+          <button id="pagButton" type="button">${i}</button>
+        </li>`
+      linkList.insertAdjacentHTML('beforeend', newButton);
     }
-  });
+    // Retrieves the button and sets the class name to 'active'
+    const buttonClass = document.querySelector('#pagButton');
+    buttonClass.className = 'active';
+    /*
+    * Listens for button clicks at the bottom of the page and changes the class to
+    * 'active' each time a button is clicked.
+    */
+    linkList.addEventListener('click', (e) => {
+      const clickButton = e.target;
+      if (clickButton.tagName === 'BUTTON') {
+        const activeButton = document.querySelector('button.active');
+        activeButton.classList.remove('active');
+        clickButton.className = 'active';
+        showPage(list, clickButton.textContent)
+      }
+    });
+  } else {
+    linkList.innerHTML = '';
+  }
 }
 // Added a search bar
 const addSearchBar =
@@ -77,9 +81,9 @@ header.insertAdjacentHTML('beforeend', addSearchBar);
 const searchInput = document.querySelector('.student-search');
 const searchButton = document.querySelector('button');
 /*
- * Creates an alert message that sits within a 'div' element and inserts
- * it into HTML file.
- */
+* Creates an alert message that sits within a 'div' element and inserts
+* it into HTML file.
+*/
 const alertMessage = document.createElement('div');
   alertMessage.className = 'alertMessage';
   alertMessage.style.color = '#1e90ff';
@@ -99,8 +103,8 @@ function removeAlert() {
   }
 }
 /*
- * Filters through the data.js file and pushes names into an empty array if
- * the users input matches the first and last name of the data information.
+* Filters through the data.js file and pushes names into an empty array if
+* the users input matches the first and last name of the data information.
 */
 function filterSearch(list) {
   const studentInput = document.querySelector('#search').value.toLowerCase();
@@ -112,9 +116,9 @@ function filterSearch(list) {
     }
   }
 /*
- * Decides that if the searchList array is empty, it will run an alertMessage
- * If the array is not emtpy it will show the amount of found searches, show
- * how many pages are needed, and removes the alertMessage
+* Decides that if the searchList array is empty, it will run an alertMessage
+* If the array is not emtpy it will show the amount of found searches, show
+* how many pages are needed, and removes the alertMessage
 */
   if (searchList.length === 0) {
     addAlertMessage();
@@ -135,7 +139,6 @@ searchButton.addEventListener('click', (e) => {
 searchInput.addEventListener('keyup', (e) => {
   filterSearch(data);
 })
-
 // Call functions
 showPage(data, 1);
 addPagination(data);
